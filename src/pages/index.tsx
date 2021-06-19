bf1304ff97096bb9f5a55fe7824043806633c078
Miguel <miguelmenezes@cpejr.com.br>
2021-06-18 23:56:46 -0300

    ChallengeBox Finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable @next/next/no-page-custom-font */
+import Head from 'next/head';
+
+
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
+import { ChallengeBox } from "../components/ChallengeBox";
 
-import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -23,7 +26,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
